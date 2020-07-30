ce129f4e718cf695acc8b8f07e74d400c3661ded
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-07-30 11:32:54 +0530

    Update test-script.py

--- test-script.py
+++ test-script.py
@@ -3,7 +3,7 @@ import requests
 url = "https://akimageanalysishack.azurewebsites.net/analyze"
 
 files = [
-  ('file', open('<your file>','rb'))
+  ('file', open('<your image file>','rb'))
 ]
 
 response = requests.request("POST", url, files = files)
